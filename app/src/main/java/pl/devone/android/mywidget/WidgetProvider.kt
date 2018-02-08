6c67b497bd5fc1641fdacbde789582fbf86e1735
ljedrzynski <lukjedrzynski@gmail.com>
2018-02-08 20:48:05 +0100

    Audioplayer - done

--- app/src/main/java/pl/devone/android/mywidget/WidgetProvider.kt
+++ app/src/main/java/pl/devone/android/mywidget/WidgetProvider.kt
@@ -9,13 +9,20 @@ import android.app.PendingIntent
 import android.content.Intent
 import android.net.Uri
 import android.content.ComponentName
-import android.preference.PreferenceManager
+import android.media.MediaPlayer
 
 
 /**
  * Implementation of App Widget functionality.
  */
 class WidgetProvider : AppWidgetProvider() {
+    companion object {
+        var mp: MediaPlayer? = null
+        var imageIdx: Int = 0
+        var songIdx: Int = 0
+    }
+
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -43,39 +50,73 @@ class WidgetProvider : AppWidgetProvider() {
             }
             "OPEN_AUDIO_PLAYER" -> {
                 updateWidgetLayout(AppWidgetManager.getInstance(context), context!!, R.layout.audio_player,
-                        hashMapOf(R.id.btn_back to getPendingSelfIntent(context, "OPEN_WIDGET_MENU")))
+                        hashMapOf(R.id.btn_back to getPendingSelfIntent(context, "OPEN_WIDGET_MENU"),
+                                R.id.btn_next_song to getPendingSelfIntent(context, "NEXT_SONG"),
+                                R.id.btn_play to getPendingSelfIntent(context, "PLAY_SONG"),
+                                R.id.btn_prev_song to getPendingSelfIntent(context, "PREV_SONG")))
             }
             "OPEN_IMAGE_EXPLORER" -> {
                 updateWidgetLayout(AppWidgetManager.getInstance(context), context!!, R.layout.image_brs,
                         hashMapOf(R.id.btn_back to getPendingSelfIntent(context, "OPEN_WIDGET_MENU"),
                                 R.id.btn_next_image to getPendingSelfIntent(context, "NEXT_IMAGE"),
-                                R.id.btn_previous_image to getPendingSelfIntent(context, "PREVIOUS_IMAGE")))
+                                R.id.btn_prev_image to getPendingSelfIntent(context, "PREV_IMAGE")))
             }
             "OPEN_WIDGET_MENU" -> {
                 setWidgetMainLayout(AppWidgetManager.getInstance(context), context!!)
             }
             "NEXT_IMAGE" -> {
                 updateWidgetLayout(AppWidgetManager.getInstance(context), context!!, RemoteViews(context.packageName, R.layout.image_brs).apply {
-                    setImageViewResource(R.id.imageView1, context.resources.getIdentifier(String.format("pic_%s", getCurrentImageIndex(context, true)),
+                    setImageViewResource(R.id.imageView1, context.resources.getIdentifier(String.format("pic_%s", ++imageIdx),
                             "drawable", context.packageName))
                 })
             }
-            "PREVIOUS_IMAGE" -> {
+            "PREV_IMAGE" -> {
                 updateWidgetLayout(AppWidgetManager.getInstance(context), context!!, RemoteViews(context.packageName, R.layout.image_brs).apply {
-                    setImageViewResource(R.id.imageView1, context.resources.getIdentifier(String.format("pic_%s", getCurrentImageIndex(context)),
+                    setImageViewResource(R.id.imageView1, context.resources.getIdentifier(String.format("pic_%s", --imageIdx),
                             "drawable", context.packageName))
                 })
+            }
+            "NEXT_SONG" -> {
+                if (mp != null) {
+                    if (mp?.isPlaying!!) {
+                        mp!!.stop()
+                        mp!!.release()
+                    }
+                }
+                MediaPlayer.create(context!!.getApplicationContext(), context.resources.getIdentifier(String.format("song_%s", ++songIdx),
+                        "raw", context.packageName)).run {
+                    mp = this
+                    start()
+                }
+            }
+            "PREV_SONG" -> {
+                if (mp != null) {
+                    if (mp?.isPlaying!!) {
+                        mp!!.stop()
+                        mp!!.release()
+                    }
+                }
+                MediaPlayer.create(context!!.getApplicationContext(), context.resources.getIdentifier(String.format("song_%s", --songIdx),
+                        "raw", context.packageName)).run {
+                    mp = this
+                    start()
+                }
+            }
+            "PLAY_SONG" -> {
+                if (mp != null) {
+                    mp?.apply {
+                        if (isPlaying) pause() else start()
+                    }
+                } else {
+                    MediaPlayer.create(context!!.getApplicationContext(), context.resources.getIdentifier(String.format("song_%s", ++songIdx),
+                            "raw", context.packageName)).run {
+                        mp = this
+                        start()
+                    }
+                }
             }
         }
     }
-
-    private fun getCurrentImageIndex(context: Context, next: Boolean = false): Int =
-            PreferenceManager.getDefaultSharedPreferences(context).let {
-                var value = it.getInt("image_browser_idx", 0)
-                it.edit().putInt("image_browser_idx", if (next) ++value else --value).apply()
-                value
-            }
-
 
     private fun updateWidgetLayout(appWidgetManager: AppWidgetManager, context: Context, remoteViews: RemoteViews) {
         appWidgetManager.updateAppWidget(ComponentName(context, WidgetProvider::class.java), remoteViews)
